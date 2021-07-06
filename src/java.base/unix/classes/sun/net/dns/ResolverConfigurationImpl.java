ec285598849a27f681ea6269342cf03cf382eb56
Duy Tran Khanh <40482367+khanhduytran0@users.noreply.github.com>
2021-07-06 18:29:24 +0700

    Allow custom /etc/resolv.conf path with ext.net.resolvPath property

--- src/java.base/unix/classes/sun/net/dns/ResolverConfigurationImpl.java
+++ src/java.base/unix/classes/sun/net/dns/ResolverConfigurationImpl.java
@@ -62,9 +62,11 @@ public class ResolverConfigurationImpl
     {
         LinkedList<String> ll = new LinkedList<>();
 
+        String resolvPath = System.getProperty("ext.net.resolvPath", "/etc/resolv.conf");
+
         try {
             BufferedReader in =
-                new BufferedReader(new FileReader("/etc/resolv.conf"));
+                new BufferedReader(new FileReader(resolvPath));
             String line;
             while ((line = in.readLine()) != null) {
                 int maxvalues = maxperkeyword;
